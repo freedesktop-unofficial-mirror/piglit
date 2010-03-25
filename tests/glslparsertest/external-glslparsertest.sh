73ee2383b4e0d60386a1c2f8b0169b117e17cab7
Eric Anholt <eric@anholt.net>
2010-03-25 16:08:27 -0700

    Improve the external glslparsertest output for better failure summaries.

--- tests/glslparsertest/external-glslparsertest.sh
+++ tests/glslparsertest/external-glslparsertest.sh
@@ -6,17 +6,28 @@ else
     type="v"
 fi
 
-~/src/glsl2/glsl $type $1 > /dev/null
+if test "x$2" != "xpass" -a "x$2" != "xfail"; then
+    echo "usage: external-glslparsertest.sh filename [pass|fail]"
+    exit 1;
+fi
+
+result=$(~/src/glsl2/glsl $type $1)
 status=$?
 
 if test "x$2" = xpass; then
     if test "$status" = "0"; then
 	echo PIGLIT: {\'result\': \'pass\' }
     else
+	echo "shader source:"
+	cat $1
+	echo "compiler result:"
+	echo "$result"
 	echo PIGLIT: {\'result\': \'fail\' }
     fi
 else
     if test "$status" = "0"; then
+	echo "shader should have failed:"
+	cat $1
 	echo PIGLIT: {\'result\': \'fail\' }
     else
 	echo PIGLIT: {\'result\': \'pass\' }
